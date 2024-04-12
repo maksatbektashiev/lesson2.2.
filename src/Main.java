2bb7180fa2f00bcd21161454e99f80ca0441cd2e
tashievmaksatbek <tashievmaksatbek@gmail.com>
2024-04-12 12:02:20 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int student = 10;
+        int tempirature = 15;
+        boolean reini = true;
+
+        if (tempirature > 5 && tempirature < 40) {
+            System.out.println("go to work");
+        }
+        if (tempirature > 25 && student > 10) {
+            System.out.println("go to work");
+        }
+        if (reini || tempirature < 13) {
+            System.out.println("go to home ");
+        }
+         if (student > 20 || tempirature>5 &&tempirature<40)
+    {
+             System.out.println("go to park");
+        }
+    }
+}
